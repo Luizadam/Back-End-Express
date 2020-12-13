f97f5dbce15e2bdbc65f5eec3d92b7d973766364
Luizadam <luizadaam@gmail.com>
2020-12-13 21:27:49 +0700

    add procfile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 // const io = require('socket.io')(port)
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
